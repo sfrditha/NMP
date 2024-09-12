6c904e580dd60b68be53e7b4041ca252b389ca0f
NatasyaAuliiaa <natasyaakr@gmail.com>
2024-09-12 12:48:20 +0700

    buttonya udah yaaa

--- app/src/main/java/com/ubaya/anative/whoWeAre.kt
+++ app/src/main/java/com/ubaya/anative/whoWeAre.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class aboutUs : AppCompatActivity() {
+class whoWeAre : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about_us)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_who_we_are)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_game2)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
