4ec26e1829253704220e51fa46aa3493a412c756
bangkitbali <s160422163@student.ubaya.ac.id>
2024-09-13 14:38:53 +0700

    schedule done

--- app/src/main/java/com/ubaya/anative/schedule.kt
+++ app/src/main/java/com/ubaya/anative/schedule.kt
@@ -1,6 +1,8 @@
 package com.ubaya.anative
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class schedule : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val test = findViewById<ImageView>(R.id.btn_notifyMe)
+        test.setOnClickListener{
+            val Intent = Intent(this,whatWePlayDetail::class.java)
+            startActivity(Intent)
         }
     }
 }
